233ce4befc9d1cbe2e93a8382d3777e240eb9f79
Grigoriy <foxtail25@gmail.com>
2025-09-16 09:49:50 +0300

    classDataBase - repit

--- public/classes/dataBaseShell.php
+++ public/classes/dataBaseShell.php
@@ -14,21 +14,33 @@
 		}
 		public function save($table, $data) {
 			// сохраняет запись в базу
+			$query = "INSERT INTO $table (name, age, salary) 
+		VALUES ('user', 30, 1000)";
 		}
 		public function del($table, $id) {
 			// удаляет запись по ее id
+			$query = "";
 		}
 		public function delAll($table, $ids) {
 			// удаляет записи по их id
+			$query = "";
 		}
 		public function get($table, $id) {
 			// получает одну запись по ее id
+			$query = "SELECT * FROM $table WHERE id = $id";
 		}
 		public function getAll($table, $ids) {
 			// получает массив записей по их id
+			$query = "";
 		}
 		public function selectAll($table, $condition) {
 			// получает массив записей по условию
+			$query = "";
+		}
+		private function getRes($query){
+			$res = mysqli_query($this->link, $query) or die(mysqli_error($link));
+			for ($data = []; $row = mysqli_fetch_assoc($res); $data[] = $row);
+			return $data;
 		}
 	}
 ?>
