1ca328565395ea80f049fe80a5d232b5cc3da8bd
Grigoriy <foxtail25@gmail.com>
2025-09-18 09:50:26 +0300

    namespace

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
-// пространств
+// пространство имён
+require_once('core/Controller.php');
+require_once('project/Controller.php');
 
+$core = new \Core\Controller;
+$project = new \Project\Controller;
+echo $core;
+echo '<br/>';
+echo $project;
 ?>
