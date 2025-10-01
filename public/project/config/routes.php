b7787abf10666ae1fd2ce0ccfb8fa5bfbe9a2fd4
Grigoriy <foxtail25@gmail.com>
2025-10-01 09:51:49 +0300

    MVC_2

--- public/project/config/routes.php
+++ public/project/config/routes.php
@@ -3,5 +3,7 @@
 	
 	return [
 		new Route('/hello/', 'hello', 'index'), // route for the welcome page, can be deleted
+		new Route('/my-page1/','page', 'show1'),
+		new Route('/my-page2/','page', 'show2')
 	];
 	
--- public/project/controllers/PageController.php
+++ public/project/controllers/PageController.php
@@ -4,6 +4,14 @@ namespace Project\Controllers;
 use \Core\Controller;
 
 class PageController extends Controller {
+
+    public function show1(){
+        echo '1';
+    }
+
+    public function show2(){
+        echo '2';
+    }
 	
 }
 
