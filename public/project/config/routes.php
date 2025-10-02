3fe75a2c9861bf84b67ba5c4dda61fe95c45ea3d
Grigoriy <foxtail25@gmail.com>
2025-10-02 09:53:34 +0300

    MVC_2

--- public/project/config/routes.php
+++ public/project/config/routes.php
@@ -4,6 +4,9 @@
 	return [
 		new Route('/hello/', 'hello', 'index'), // route for the welcome page, can be deleted
 		new Route('/my-page1/','page', 'show1'),
-		new Route('/my-page2/','page', 'show2')
+		new Route('/my-page2/','page', 'show2'),
+		new Route('/my-act1/','test', 'act1'),
+		new Route('/my-act2/','test', 'act2'),
+		new Route('/my-act3/','test', 'act3'),
 	];
 	
