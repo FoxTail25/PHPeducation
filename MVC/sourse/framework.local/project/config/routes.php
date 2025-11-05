e9082ce9148cafd3407782ed3809353bfc48b4ce
Fox Tail <foxtail25@gmail.com>
2025-11-05 12:11:49 +0300

    restart_2

---
+++ MVC/sourse/framework.local/project/config/routes.php
@@ -0,0 +1,7 @@
+<?php
+	use \Core\Route;
+	
+	return [
+		new Route('/hello/', 'hello', 'index'), // route for the welcome page, can be deleted
+	];
+	
---
+++ MVC/sourse/framework.local/project/views/error/notFound.php
@@ -0,0 +1,7 @@
+<p>
+	The page you requested was not found!
+</p>
+<p>
+	Most likely, the reason is that there is no route defined for this URL
+	in the route file located at <i>/project/config/routes.php</i>.
+</p>
